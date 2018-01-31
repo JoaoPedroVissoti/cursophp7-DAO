f9aa4521295d4ce9586ef9a915851af2ae55f62a
Joao Pedro <joaovissoti@gmail.com>
2018-01-30 22:11:16 -0200

    metodos de lista

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -57,6 +57,19 @@ class Usuario{
 		}
 	}
 
+	public static function getlist(){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+	}
+
+	public static function search($login){
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%"
+		));
+	}
+
 	public function __toString(){
 
 		return json_encode(array(
--- DAO/index.php
+++ DAO/index.php
@@ -2,11 +2,18 @@
 
 require_once("config.php");
 
-$joao = new Usuario();
+//carrega um usuário
+//$joao = new Usuario();
+//$joao->loadbyID(1);
+//echo $joao;
 
-$joao->loadbyID(1);
 
-echo $joao;
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
 
+//Carrega uma lista de usuarios pelo login
+$search = Usuario::search("jo");
+echo json_encode($search);
  ?>
